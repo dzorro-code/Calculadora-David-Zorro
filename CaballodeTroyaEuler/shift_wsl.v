32712232e0ec4b0eba008532b4f16382d2069683
dzorro-code <dzorro@unal.edu.co>
2026-07-16 22:39:01 -0500

    Proyecto terminado, ejecutar make clean syn config

--- CaballodeTroyaEuler/shift_wsl.v
+++ CaballodeTroyaEuler/shift_wsl.v
@@ -1,12 +1,12 @@
 module shift_wsl(clk , in_RGB , o_RGB, sh , ld);
-  input clk;
-  input ld;
-  input sh;
+input clk;
+input ld;
+input sh;
 
-  input [23:0]in_RGB;
-  output reg [23:0]o_RGB;
+input [23:0]in_RGB;
+output reg [23:0]o_RGB;
 
-  always @(negedge clk) begin
+always @(negedge clk) begin
     if (ld) begin
         o_RGB[23:0] <= in_RGB[23:0];
     end
@@ -15,7 +15,7 @@ module shift_wsl(clk , in_RGB , o_RGB, sh , ld);
             o_RGB[23:0] <= {o_RGB[22:0], 1'b0 };
         end
             else begin
-              o_RGB   <= o_RGB;
+                o_RGB   <= o_RGB;
             end
         
     end
